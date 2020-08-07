25c5f5570a85b0901efe9e03d04123eaef7e9ff6
emiledehn <69158878+emiledehn@users.noreply.github.com>
2020-08-07 15:13:51 +0200

    test

--- TEST/resources/js/index.js
+++ TEST/resources/js/index.js
@@ -1,10 +1,9 @@
-var mouse = {
-	x: undefined,
-	y: undefined
-}
 
-function onMouseDown(event) {
-	
+
+//////////////////////////////////////////////////////////////////////////////
+// MOUSE HANDLING
+var mouse = {
+    x: undefined, y: undefined
 }
 
 function onMouseMove(event) {
@@ -15,69 +14,104 @@ function onMouseMove(event) {
         mouse.y = undefined;
     } 
 }
+//////////////////////////////////////////////////////////////////////////////
 
-function movingCircle(x,y,radius,dx,dy,color) {
-	this.x=x;
-    this.y=y; 
-    this.dx=dx;
-    this.dy=dy;
-    this.radius=radius;
-	this.color=color;
-	this.circle;
 
-	this.draw = function() {
-		this.circle = new Path.Circle(this.x,this.y,this.radius);
-		this.circle.fillColor = color;
-	}
-	this.update = function() {
-		if(this.circle.position.x+this.radius>paper.view.bounds.width || this.circle.position.x-radius<0) {
-            this.dx = -this.dx;
-        }
-        if(this.circle.position.y+this.radius>paper.view.bounds.height || this.circle.position.y-radius<0) {
-            this.dy = -this.dy;
-        }
-        this.circle.position.x+=this.dx;
-        this.circle.position.y+=this.dy;
-        this.circle.fillColor.hue -= 3;
-		
-		
-        var distance = 100;
-        if(mouse.x - this.circle.position.x < distance && mouse.x -this.circle.position.x > -distance && mouse.y - this.circle.position.y < distance && mouse.y - this.circle.position.y > -distance) {
-			if(this.circle.bounds.width/2 < radius*5){
-				this.circle.scale(2);
-            } 
-		} else if(this.circle.bounds.width/2 > radius) {
-			this.circle.scale(0.95);
-        }
-	}
+
+//////////////////////////////////////////////////////////////////////////////
+// SCALING
+var originalSize = view.bounds.width;
+var originalSize2 = view.bounds.width;
+var sizeFactor;
+var sizeFactor2;
+var norm = 1 + ((view.bounds.width - 1024) / 1024);
+
+
+
+function onResize(event) {
+    sizeFactor = 1 + (view.bounds.width - originalSize) / originalSize;
+    sizeFactor2 = 1 + (view.bounds.width - originalSize2) / originalSize2;
+    originalSize = view.bounds.width;
+  
+    console.log("hi");
+
+    //circle resizing
+    circles.scale(sizeFactor);
+    circles.position = view.center;
 }
+//////////////////////////////////////////////////////////////////////////////
 
+var circleAmount = 400;
 
-var circleArray = [];
 function init() {
-    circleArray=[];
-	for(var i=0; i<500; i++) {
-        var color = random_rgba();
-        var radius = Math.random() * 20;
-        var x = Math.random() * (view.bounds.width - radius*2) + radius;
-        var y = Math.random() * (view.bounds.height - radius*2) + radius;
-        var dy = (Math.random() - 0.5) * 7;
-        var dx = (Math.random() - 0.5) * 7;
-		circleArray.push(new movingCircle(x, y, radius, dx, dy, color));
-		circleArray[i].draw();
-    }
-   
+    spawnCircles(circleAmount);
 }
 
 function onFrame(event) {
-    for(var i=0; i<circleArray.length; i++) {
-        circleArray[i].update();
+    moveCircles();
+}
+
+
+var circRadius = [];
+var circles = new Group;
+
+function spawnCircles() {
+    for(var i=0; i<circleAmount; i++) {
+        var color = random_rgba();
+        var radius = (Math.random() * 20 + 10) * norm;
+        var x = Math.random() * (view.bounds.width - radius*2*norm) + radius*norm;
+        var y = Math.random() * (view.bounds.height - radius*2*norm) + radius*norm;
+        circRadius.push(radius);
+        circles.children.push(new Path.Circle(x, y, radius));
+        circles.children[i].fillColor = color;
+    }
+}
+
+var dx = [];
+var dy = [];
+for(var i=0; i<circleAmount; i++) {
+    dx.push((Math.random() - 0.5) * 5);
+    dy.push((Math.random() - 0.5) * 5);
+}
+
+function moveCircles() {
+    for(var i=0; i<circleAmount; i++) {
+        if(circles.children[i].position.x+radius(circles.children[i])>paper.view.bounds.width || circles.children[i].position.x-radius(circles.children[i])<0) {
+            dx[i] = -dx[i];
+        }
+        if(circles.children[i].position.y+radius(circles.children[i])>paper.view.bounds.height || circles.children[i].position.y-radius(circles.children[i])<0) {
+            dy[i] = -dy[i];
+        }
+        circles.children[i].position.x += dx[i];
+        circles.children[i].position.y += dy[i];
+    }
+    for(var i=0; i<circleAmount; i++) {
+        var distance = 75 * sizeFactor2;
+        if(mouse.x - circles.children[i].position.x < distance && mouse.x -circles.children[i].position.x > -distance && mouse.y - circles.children[i].position.y < distance && mouse.y - circles.children[i].position.y > -distance) {
+            circles.children[i].fillColor.hue -=2;
+            if(radius(circles.children[i]) < circRadius[i]*3*sizeFactor2){
+                circles.children[i].scale(2);
+            
+            } 		
+        } else if(radius(circles.children[i]) > circRadius[i]*sizeFactor2) {
+            circles.children[i].scale(0.95);
+            //if(circles.children[i].fillColor.hue != 250 & circles.children[i].fillColor.hue >= -110) {
+            //    circles.children[i].fillColor.hue -=10;
+            //}
+
+            // 0 rot
+            // 250 blau
+        }
     }
 }
 
 function random_rgba() {
     var o = Math.round, r = Math.random, s = 255;
     return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
+function radius(path) {
+    return (path.bounds.width / 2 + path.strokeWidth / 2);
 }
 
 init();
